d796b23ee6e6a19cb50bfda7bf99ab9fbebb1d4f
clayo24 <clay_olson@asl.org>
2023-10-13 10:58:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# duetVersion3
+ It's version 3 because github sucks and hopefully this works
---
+++ duetVersion3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  duetVersion3
+//
+//  Created by Clay Olson on 10/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
