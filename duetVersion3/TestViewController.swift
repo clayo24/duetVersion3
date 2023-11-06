943a37a3ab08fc9c68d084e244b8c9cf1d70561a
clayo24 <clay_olson@asl.org>
2023-11-06 11:41:35 +0000

    Fixing some things
    Trying to add screen transitions

---
+++ duetVersion3/TestViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TestViewController.swift
+//  duetVersion3
+//
+//  Created by Clay Olson on 11/2/23.
+//
+
+import SwiftUI
+
+struct TestViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestViewController()
+}
